30aabed65b8940a5876b09ac884aeff40a78da65
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2024-08-16 11:48:25 +1000

    added image compile

---
+++ symfony/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.{vue,js,ts,tsx,jsx}",
+    "./templates/**/*.{html,twig}"
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
