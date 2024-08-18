35f4e8906a2e8f60fa9aefe27911b5f73ef99a73
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2024-08-18 14:44:26 +1000

    Create form

--- symfony/src/Controller/TeachersController.php
+++ symfony/src/Controller/TeachersController.php
@@ -2,26 +2,78 @@
 
 namespace App\Controller;
 
+use App\Entity\Teacher;
+use App\Form\TeacherFormType;
 use PhpParser\Node\Expr\Cast\Object_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\TeacherRepository;
-
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class TeachersController extends AbstractController
 {
+    private $em;//entity manager
 
     //Connect to the entries for teachers in the DB
     private $teacherRepository;
-    public function __construct(TeacherRepository $teacherRepository)
+    public function __construct(TeacherRepository $teacherRepository, EntityManagerInterface $em )
     {
         $this->teacherRepository = $teacherRepository;
+        $this->em = $em;
 
     }
 
-    // Show all
+    //CREATE
+    #[Route('teachers/create', name: 'create_teacher')]
+    public function create(Request $request): Response
+    {
+        $teacher = new Teacher();
+        $form = $this->createForm(TeacherFormType::class, $teacher);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+          $newTeacher = $form->getData();
+         
+          //Deal with image paths
+          $imagePath = $form->get('imagePath')->getData();
+          if ($imagePath) {
+            $newFileName = uniqid() . "." . $imagePath->guessExtension();
+
+            try {
+              $imagePath->move(
+                //Accepts 2 params:
+                // Where to look
+                $this->getParameter('kernel.project_dir') . '/public/uploads',
+                //New file name
+                $newFileName
+              );
+            } catch  (FileException $e){
+              return new Response($e->getMessage());
+            }
+
+            $teacher->setImagePath('/uploads/' . $newFileName);
+
+          }
+
+          $this->em->persist($newTeacher);
+          $this->em->flush();
+
+          return $this->redirect('/teachers');
+
+
+        }
+
+        return $this->render('teachers/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
+
+    // READ all
     #[Route('/teachers', methods:['GET'], name: 'teachers')]
     public function index(): Response
     {
@@ -33,7 +85,7 @@ class TeachersController extends AbstractController
 
     }
 
-    // Show one
+    // READ one
     #[Route('/teachers/{id}', methods:['GET'], name: 'teacher')]
     public function show($id): Response
     {
