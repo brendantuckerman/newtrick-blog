e0a1a33ce720149b3d4f747a6bc048f41320a946
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2024-09-02 10:30:53 +1000

    Edit form styles pre-merge with work in create

--- symfony/src/Form/PostFormType.php
+++ symfony/src/Form/PostFormType.php
@@ -23,25 +23,27 @@ class PostFormType extends AbstractType
                 'attr' => array(
                   'class' => 'bg-transparent p-8 m8 block border-b-2 w-full h-20 text-6xl outline-none',
                   'placeholder' => 'Give this post a title'
-                  )
-                ]
-            )
+                ),
+                'label_attr' => ['class' => 'post-form-label'],
+                'row_attr' => ['class' => 'post-form-row-wrapper'],
+            ])
             ->add('imagePath', FileType::class,  [
                 'required' => false,
                 'mapped' => false,
                 'label' => 'Image',
                 'attr' => array(
                   'class' => 'py-10 block'
-                  )
-                ]
-            )
+                ),
+                'label_attr' => ['class' => 'post-form-label'],
+                'row_attr' => ['class' => 'post-form-row-wrapper']
+            ])
             ->add('content',  CKEditorType::class, [
                 'config_name' => 'my_config',
                 'required' => false,
                 'attr' => ['class' => 'ckeditor'],
-               
-              ]
-            )
+                'label_attr' => ['class' => 'post-form-label'],
+                'row_attr' => ['class' => 'post-form-row-wrapper']
+            ])
             ->add('tags', CollectionType::class, [
                 'entry_type' => TextType::class,
                 'allow_add' => true,
@@ -50,15 +52,20 @@ class PostFormType extends AbstractType
                 'required' => false,
                 'label' => 'Tags',
                 'by_reference' => false,
-                ]
-            )
+                'label_attr' => ['class' => 'post-form-label'],
+                'row_attr' => ['class' => 'post-form-row-wrapper']
+            ])
             ->add('user', EntityType::class, [
                 'class' => User::class,
-                'choice_label' => 'email'
+                'choice_label' => 'email',
+                'label_attr' => ['class' => 'post-form-label'],
+                'row_attr' => ['class' => 'post-form-row-wrapper'],
             ])
             ->add('published', CheckboxType::class, [
                 'required' => false,
                 'label' => 'Publish this post',
+                'label_attr' => ['class' => 'post-form-label'],
+                'row_attr' => ['class' => 'post-form-row-wrapper'],
             ])
         ;
     }
